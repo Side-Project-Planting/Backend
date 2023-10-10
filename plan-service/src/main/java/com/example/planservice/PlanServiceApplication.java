2f4730b17a6cbe35114abcf2c954af33af7e6e84
kimtaehoonDev <thkimm.dev@gmail.com>
2023-10-10 19:43:17 +0900

    chore: Plan Service 생성

---
+++ plan-service/src/main/java/com/example/planservice/PlanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.planservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanServiceApplication.class, args);
+    }
+
+}
