cee27c2b2eb0e169eea7a02890ee8e71bc46afbe
kimtaehoonDev <thkimm.dev@gmail.com>
2023-10-24 20:09:32 +0900

    feat: DELETE /labels/{labelId} 매핑

--- plan-service/src/main/java/com/example/planservice/presentation/LabelController.java
+++ plan-service/src/main/java/com/example/planservice/presentation/LabelController.java
@@ -3,13 +3,17 @@ package com.example.planservice.presentation;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.planservice.application.LabelService;
+import com.example.planservice.application.dto.LabelDeleteServiceRequest;
 import com.example.planservice.presentation.dto.request.LabelCreateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,5 +29,19 @@ public class LabelController {
                                 @RequestAttribute Long userId) {
         Long createdId = labelService.create(userId, labelCreateRequest);
         return ResponseEntity.created(URI.create("/labels/" + createdId)).build();
+    }
+
+    @DeleteMapping("/{labelId}")
+    ResponseEntity<Void> delete(@PathVariable Long labelId,
+                                @RequestParam Long planId,
+                                @RequestAttribute Long userId) {
+        LabelDeleteServiceRequest request = LabelDeleteServiceRequest.builder()
+            .labelId(labelId)
+            .planId(planId)
+            .memberId(userId)
+            .build();
+
+        labelService.delete(request);
+        return ResponseEntity.noContent().build();
     }
 }
--- plan-service/src/main/java/com/example/planservice/presentation/dto/request/LabelCreateRequest.java
+++ plan-service/src/main/java/com/example/planservice/presentation/dto/request/LabelCreateRequest.java
@@ -1,5 +1,7 @@
 package com.example.planservice.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class LabelCreateRequest {
+    @NotBlank
     private String name;
+
+    @NotNull
     private Long planId;
 
     @Builder
