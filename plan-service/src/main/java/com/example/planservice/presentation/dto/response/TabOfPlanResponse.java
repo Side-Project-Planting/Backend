5eacbd3c438cf69ee18af75471cf1e3f6e180f96
wooseong <dominance5995@gmail.com>
2023-11-02 11:51:52 +0900

    feat: DTO 설정

---
+++ plan-service/src/main/java/com/example/planservice/presentation/dto/response/TabOfPlanResponse.java
@@ -0,0 +1,31 @@
+package com.example.planservice.presentation.dto.response;
+
+import java.util.List;
+
+import com.example.planservice.domain.tab.Tab;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class TabOfPlanResponse {
+    private Long id;
+    private String title;
+    private List<Long> taskOrder;
+
+    @Builder
+    private TabOfPlanResponse(Long id, String title, List<Long> taskOrder) {
+        this.id = id;
+        this.title = title;
+        this.taskOrder = taskOrder;
+    }
+
+    public TabOfPlanResponse toPlanResponse(Tab tab, List<Long> taskOrder) {
+        return builder()
+            .id(tab.getId())
+            .title(tab.getName())
+            .taskOrder(taskOrder)
+            .build();
+    }
+}
---
+++ plan-service/src/main/java/com/example/planservice/presentation/dto/response/TaskOfPlanResponse.java
@@ -0,0 +1,45 @@
+package com.example.planservice.presentation.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.planservice.domain.task.Task;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TaskOfPlanResponse {
+    private Long id;
+    private String title;
+    private List<Long> labels;
+    private Long tabId;
+    private Long assigneeId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    @Builder
+    private TaskOfPlanResponse(Long id, String title, Long tabId, List<Long> labels, Long assigneeId,
+                               LocalDateTime startDate, LocalDateTime endDate) {
+        this.id = id;
+        this.title = title;
+        this.labels = labels;
+        this.tabId = tabId;
+        this.assigneeId = assigneeId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public TaskOfPlanResponse toPlanResponse(Task task) {
+        return builder()
+            .id(task.getId())
+            .title(task.getName())
+            .labels(task.getLabelOfTasks().stream().map(labelOfTask -> labelOfTask.getLabel().getId()).toList())
+            .tabId(task.getTab().getId())
+            .assigneeId(task.getWriter().getId())
+            .startDate(task.getStartDate())
+            .endDate(task.getEndDate())
+            .build();
+    }
+}
