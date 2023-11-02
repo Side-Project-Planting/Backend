fed6230e5d80220abee41dff660e2ee02d72f2f1
wooseong <dominance5995@gmail.com>
2023-11-02 11:52:49 +0900

    feat: Plan 조회 로직 추가

--- plan-service/src/main/java/com/example/planservice/application/PlanService.java
+++ plan-service/src/main/java/com/example/planservice/application/PlanService.java
@@ -1,19 +1,30 @@
 package com.example.planservice.application;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.planservice.domain.Linkable;
 import com.example.planservice.domain.member.Member;
 import com.example.planservice.domain.member.repository.MemberRepository;
 import com.example.planservice.domain.plan.Plan;
 import com.example.planservice.domain.plan.repository.PlanRepository;
 import com.example.planservice.domain.tab.Tab;
 import com.example.planservice.domain.tab.repository.TabRepository;
+import com.example.planservice.domain.task.Task;
 import com.example.planservice.exception.ApiException;
 import com.example.planservice.exception.ErrorCode;
 import com.example.planservice.presentation.dto.request.PlanCreateRequest;
+import com.example.planservice.presentation.dto.response.LabelOfPlanResponse;
+import com.example.planservice.presentation.dto.response.MemberOfPlanResponse;
+import com.example.planservice.presentation.dto.response.PlanResponse;
+import com.example.planservice.presentation.dto.response.TabOfPlanResponse;
+import com.example.planservice.presentation.dto.response.TaskOfPlanResponse;
 import lombok.RequiredArgsConstructor;
 
 
@@ -45,6 +56,43 @@ public class PlanService {
         return savedPlan.getId();
     }
 
+    @Transactional
+    public PlanResponse getTotalPlanResponse(Long planId) {
+        Plan plan = planRepository.findById(planId).orElseThrow(() -> new ApiException(ErrorCode.PLAN_NOT_FOUND));
+        List<MemberOfPlanResponse> members = plan.getMembers().parallelStream().map(member ->
+            new MemberOfPlanResponse().toPlanResponse(member.getMember(), plan.getOwner().getId())
+        ).toList();
+
+        List<Tab> tabList = plan.getTabs();
+
+        List<Long> tabOrder = orderByNext(tabList);
+
+        List<TabOfPlanResponse> tabs = tabList.parallelStream().map(tab -> {
+            List<Task> tasksOfTab = tab.getTasks();
+            List<Long> taskOrder = orderByNext(tasksOfTab);
+            return new TabOfPlanResponse().toPlanResponse(tab, taskOrder);
+        }).toList();
+
+        List<LabelOfPlanResponse> labels =
+            plan.getLabels().stream().map(label -> new LabelOfPlanResponse().toPlanResponse(label)
+            ).toList();
+
+        List<TaskOfPlanResponse> tasks =
+            plan.getTasks().stream().map(task -> new TaskOfPlanResponse().toPlanResponse(task)
+            ).toList();
+
+        return PlanResponse.builder()
+            .title(plan.getTitle())
+            .description(plan.getIntro())
+            .members(members)
+            .tabOrder(tabOrder)
+            .tabs(tabs)
+            .tasks(tasks)
+            .labels(labels)
+            .isPublic(plan.isPublic())
+            .build();
+    }
+
     private void sendInviteMail(List<String> invitedEmails, String title) {
         invitedEmails.forEach(email -> emailService.sendEmail(email, title));
     }
@@ -55,6 +103,31 @@ public class PlanService {
         firstTab.connect(lastTab);
         tabRepository.save(firstTab);
         tabRepository.save(lastTab);
+    }
+
+    @Transactional
+    public <T extends Linkable<T>> List<Long> orderByNext(List<T> items) {
+        List<Long> orderedItems = new ArrayList<>();
+        Hibernate.initialize(items);
+        Set<T> allNodes = new HashSet<>(items);
+
+        T start = null;
+        for (T item : items) {
+            if (item.getNext() != null) {
+                allNodes.remove(item.getNext());
+            }
+        }
+        if (!allNodes.isEmpty()) {
+            start = allNodes.iterator().next();
+        }
+
+        T current = start;
+        while (current != null) {
+            orderedItems.add(current.getId());
+            current = current.getNext();
+        }
+
+        return orderedItems;
     }
 
 }
--- plan-service/src/main/java/com/example/planservice/presentation/PlanController.java
+++ plan-service/src/main/java/com/example/planservice/presentation/PlanController.java
@@ -4,7 +4,10 @@ import java.net.URI;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.planservice.application.PlanService;
 import com.example.planservice.presentation.dto.request.PlanCreateRequest;
-
+import com.example.planservice.presentation.dto.response.PlanResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -30,5 +33,10 @@ public class PlanController {
         }
         Long createdId = planService.create(request, userId);
         return ResponseEntity.created(URI.create("/plans/" + createdId)).build();
+    }
+
+    @GetMapping("/{planId}")
+    public ResponseEntity<PlanResponse> read(@PathVariable Long planId, @RequestAttribute Long userId) {
+        return ResponseEntity.ok(planService.getTotalPlanResponse(planId));
     }
 }
