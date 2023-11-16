e4d021fd8004ea47658fed7348e267f6da82a44f
kimtaehoonDev <thkimm.dev@gmail.com>
2023-11-16 18:49:44 +0900

    chore: PlanServiceApplication에서 Configuration 설정 분리

--- plan-service/src/main/java/com/example/planservice/PlanServiceApplication.java
+++ plan-service/src/main/java/com/example/planservice/PlanServiceApplication.java
@@ -2,12 +2,8 @@ package com.example.planservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableJpaAuditing
 public class PlanServiceApplication {
 
     public static void main(String[] args) {
