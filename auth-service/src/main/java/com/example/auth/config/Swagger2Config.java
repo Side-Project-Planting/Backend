f1dff3b083ce4aec22ecb0bfc4c99c6591f72923
kimtaehoonDev <thkimm.dev@gmail.com>
2023-11-27 17:41:47 +0900

    refactor: 인증 서버 Swagger 관련 정보 yml로 이동

--- auth-service/src/main/java/com/example/auth/config/Swagger2Config.java
+++ auth-service/src/main/java/com/example/auth/config/Swagger2Config.java
@@ -1,29 +1,33 @@
 package com.example.auth.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
+@OpenAPIDefinition
 @Configuration
 public class Swagger2Config {
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-            .group("v1")
-            .pathsToMatch("/**")
-            .build();
-    }
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI(
+        @Value("${openapi.service.title}") String serviceTitle,
+        @Value("${openapi.service.version}") String serviceVersion,
+        @Value("${openapi.service.url}") String url,
+        @Value("${openapi.service.description}") String description) {
         Info info = new Info()
-            .title("인증 서버")
-            .version("v0.0.1")
-            .description("Planting 서버 중 인증 서버의 API 명세입니다");
-        return new OpenAPI().info(info);
+            .title(serviceTitle)
+            .version(serviceVersion)
+            .description(description);
+        return new OpenAPI()
+            .servers(List.of(new Server().url(url)))
+            .info(info);
     }
 
 }
