e15d98e78e3a6914b15477061d0bfe2efe4dd45e
kimtaehoonDev <thkimm.dev@gmail.com>
2023-12-17 16:41:34 +0900

    fix: 탭 삭제 시 발생하는 "태스크가 존재하지 않는다" 오류 해결

--- plan-service/src/main/java/com/example/planservice/domain/task/repository/TaskRepository.java
+++ plan-service/src/main/java/com/example/planservice/domain/task/repository/TaskRepository.java
@@ -3,6 +3,8 @@ package com.example.planservice.domain.task.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.planservice.domain.task.Task;
@@ -10,4 +12,8 @@ import com.example.planservice.domain.task.Task;
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByTabId(Long tabId);
+
+    @Modifying
+    @Query("update Task t set t.isDeleted = true where t.tab.id = :tabId")
+    void deleteAllByTabId(Long tabId);
 }
